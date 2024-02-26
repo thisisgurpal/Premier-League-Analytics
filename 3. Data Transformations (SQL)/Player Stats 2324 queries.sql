b5b9379bda3500091bdf3d9bdc2e89773480976a
Gurpal Gohler <gurpal.gohler@hotmail.co.uk>
2024-02-26 17:56:52 +0000

    Added some queries to answer for Player Stats

---
+++ 3. Data Transformations (SQL)/Player Stats 2324 queries.sql
@@ -0,0 +1,18 @@
+/* Player Stats Queries */
+
+/* Who in the Premier League Season 2023/24 has the highest pass percentage? */
+
+
+/* Who in the Premier League Season 2023/24 has made the most apperances? */
+
+
+/* Which nationality in the Premier League Season 2023/24 has the highest Tackle Success? */
+
+
+/* Which nationality in the Premier League Season 2023/24 has the highest Shooting Accuracy? */
+
+
+/* Which team in the Premier League Season 2023/24 has the highest average player age? */
+
+
+/* What is the average 'Height' by player 'Position' in the Premier League Season 2023/24? */
