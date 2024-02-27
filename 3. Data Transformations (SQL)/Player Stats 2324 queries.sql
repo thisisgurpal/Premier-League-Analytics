4971b8ab2fd367e28a91fdec9c662fc025b326a8
Gurpal Gohler <gurpal.gohler@hotmail.co.uk>
2024-02-27 12:08:10 +0000

    added more player stats queries

--- 3. Data Transformations (SQL)/Player Stats 2324 queries.sql
+++ 3. Data Transformations (SQL)/Player Stats 2324 queries.sql
@@ -7,7 +7,7 @@ SELECT
 	[Team Name],
 	[Passes per Match]
 FROM [Premier League Scrape].[dbo].[player_stats_2324_clean]
-WHERE [Team Id] IS NOT NULL
+WHERE [Team Id] IS NOT NULL AND [Passes per Match] IS NOT NULL
 ORDER BY [Passes per Match] desc
 
 /* Who currently in the Premier League Season 2023/24 has made the most apperances? */
@@ -44,7 +44,7 @@ SELECT * FROM
 WHERE [Age] < 20
 ORDER BY [Appearances] desc;
 
-/* What is the average 'Height' by player 'Position' for players that have been in the Premier League Season 2023/24? */
+/* Which position has the tallest average height for players involved in the Premier League Season 2023/24? */
 SELECT
 	[Position],
 	AVG([Height]) AS 'Average Height'
@@ -61,4 +61,87 @@ FROM [Premier League Scrape].[dbo].[player_stats_2324_clean]
 WHERE Height IS NOT NULL AND [Team Id] IS NOT NULL
 GROUP BY [Team Name]
 ORDER BY [Average Height] desc
+
+/* Do taller players make more tackles? */
+SELECT
+	A.[Height Group],
+	AVG(A.[Tackles]) as 'Average Tackles'
+FROM (
+SELECT
+	CASE 
+        WHEN [Height] < 155 THEN
+			'Less than 155'
+		WHEN [Height] >= 155 AND [Height] < 160 THEN 
+            '155 - 159'
+        WHEN [Height] >= 160 AND [Height] < 165 THEN 
+            '160 - 164'
+		WHEN [Height] >= 165 AND [Height] < 170 THEN 
+            '165 - 169'
+		WHEN [Height] >= 170 AND [Height] < 175 THEN 
+            '170 - 174'
+		WHEN [Height] >= 175 AND [Height] < 180 THEN 
+            '175 - 179'
+		WHEN [Height] >= 180 AND [Height] < 185 THEN 
+            '180 - 184'
+		WHEN [Height] >= 185 AND [Height] < 190 THEN 
+            '185 - 189'
+		WHEN [Height] >= 190 AND [Height] < 195 THEN 
+            '190 - 194'
+		WHEN [Height] >= 195 AND [Height] < 200 THEN 
+            '195 - 199'
+		WHEN [Height] >= 200 THEN 
+            'More than 200'
+	END AS 'Height Group',
+	[Tackles]
+FROM [Premier League Scrape].[dbo].[player_stats_2324_clean]
+WHERE [Tackles] IS NOT NULL AND [Height] IS NOT NULL
+) AS A
+GROUP BY A.[Height Group]
+ORDER BY [Average Tackles] desc
+
+/* Do taller players make better tackles? */
+SELECT
+	A.[Height Group],
+	AVG(A.[Tackle Success]) as 'Average Tackle Success'
+FROM (
+SELECT
+	CASE 
+        WHEN [Height] < 155 THEN
+			'Less than 155'
+		WHEN [Height] >= 155 AND [Height] < 160 THEN 
+            '155 - 159'
+        WHEN [Height] >= 160 AND [Height] < 165 THEN 
+            '160 - 164'
+		WHEN [Height] >= 165 AND [Height] < 170 THEN 
+            '165 - 169'
+		WHEN [Height] >= 170 AND [Height] < 175 THEN 
+            '170 - 174'
+		WHEN [Height] >= 175 AND [Height] < 180 THEN 
+            '175 - 179'
+		WHEN [Height] >= 180 AND [Height] < 185 THEN 
+            '180 - 184'
+		WHEN [Height] >= 185 AND [Height] < 190 THEN 
+            '185 - 189'
+		WHEN [Height] >= 190 AND [Height] < 195 THEN 
+            '190 - 194'
+		WHEN [Height] >= 195 AND [Height] < 200 THEN 
+            '195 - 199'
+		WHEN [Height] >= 200 THEN 
+            'More than 200'
+	END AS 'Height Group',
+	[Tackle Success]
+FROM [Premier League Scrape].[dbo].[player_stats_2324_clean]
+WHERE [Tackle Success] IS NOT NULL AND [Height] IS NOT NULL
+) AS A
+GROUP BY A.[Height Group]
+ORDER BY [Average Tackle Success] desc
+
+/* Reviewing player height and tackle success for players involved in the Premier League Season 2023/24? */
+SELECT
+	[Height],
+	[Tackle Success]
+FROM [Premier League Scrape].[dbo].[player_stats_2324_clean]
+WHERE [Tackle Success] IS NOT NULL AND [Height] IS NOT NULL
+
+
 
